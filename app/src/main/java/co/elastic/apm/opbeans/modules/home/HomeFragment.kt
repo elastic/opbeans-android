383990ad282ef92d840f27010f9d333946315855
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-06-30 11:09:51 +0200

    Creating HomeViewModel

--- app/src/main/java/co/elastic/apm/opbeans/modules/home/HomeFragment.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/home/HomeFragment.kt
@@ -3,9 +3,13 @@ package co.elastic.apm.opbeans.modules.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import co.elastic.apm.opbeans.R
+import co.elastic.apm.opbeans.modules.home.ui.HomeViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/home/ui/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package co.elastic.apm.opbeans.modules.home.ui
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
