2745a7ca57b37945d7bd5c5c7edf583f2c521594
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-06-30 14:32:38 +0200

    Setting up ProductListAdapter

---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/home/ui/products/ProductListAdapter.kt
@@ -0,0 +1,30 @@
+package co.elastic.apm.opbeans.modules.home.ui.products
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import co.elastic.apm.opbeans.app.data.models.Product
+
+class ProductListAdapter() : ListAdapter<Product, ProductViewHolder>(diffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.setData(getItem(position))
+    }
+
+    companion object {
+        private val diffCallback = object : DiffUtil.ItemCallback<Product>() {
+
+            override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
