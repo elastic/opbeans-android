d273a15490f7f0d09e9da33ad9a55824d236773d
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-06-30 10:31:34 +0200

    Adding HomeFragment to HomeActivity

--- app/src/main/java/co/elastic/apm/opbeans/HomeActivity.kt
+++ app/src/main/java/co/elastic/apm/opbeans/HomeActivity.kt
@@ -1,11 +1,17 @@
 package co.elastic.apm.opbeans
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.elastic.apm.opbeans.modules.home.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        if (savedInstanceState != null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, HomeFragment::class.java, null)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package co.elastic.apm.opbeans.modules.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import co.elastic.apm.opbeans.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
