c71cd019676bb01ea534c199c77465b95570081b
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-06-30 17:29:52 +0200

    Optimizing remote data mapping

--- app/src/main/java/co/elastic/apm/opbeans/app/data/source/RemoteProductSource.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/source/RemoteProductSource.kt
@@ -4,14 +4,16 @@ import co.elastic.apm.opbeans.BuildConfig
 import co.elastic.apm.opbeans.app.data.models.Product
 import co.elastic.apm.opbeans.app.data.remote.OpBeansService
 import co.elastic.apm.opbeans.app.data.remote.models.RemoteProduct
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RemoteProductSource @Inject constructor(private val opBeansService: OpBeansService) {
 
-    suspend fun getProducts(): List<Product> {
-        return opBeansService.getProducts().map {
+    suspend fun getProducts(): List<Product> = withContext(Dispatchers.IO) {
+        opBeansService.getProducts().map {
             remoteToProduct(it)
         }
     }
