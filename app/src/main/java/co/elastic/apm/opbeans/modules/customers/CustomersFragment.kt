bf57b8dc6b340c66665e0620b5b6a1f66f09baaf
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-04 11:45:39 +0200

    Creating CustomersFragment

---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/customers/CustomersFragment.kt
@@ -0,0 +1,13 @@
+package co.elastic.apm.opbeans.modules.customers
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import co.elastic.apm.opbeans.R
+
+class CustomersFragment : Fragment(R.layout.fragment_customers) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
