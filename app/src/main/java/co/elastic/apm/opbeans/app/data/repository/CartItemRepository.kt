6bc222151ef61c3d74c48c9c91aee299c7c0d56d
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-05 17:36:42 +0200

    Created CartItemRepository

---
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/repository/CartItemRepository.kt
@@ -0,0 +1,15 @@
+package co.elastic.apm.opbeans.app.data.repository
+
+import co.elastic.apm.opbeans.app.data.models.CartItem
+import co.elastic.apm.opbeans.app.data.source.LocalCartItemSource
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlinx.coroutines.flow.Flow
+
+@Singleton
+class CartItemRepository @Inject constructor(private val localCartItemSource: LocalCartItemSource) {
+
+    fun getAllCartItems(): Flow<List<CartItem>> {
+        return localCartItemSource.getAllCartItems()
+    }
+}
