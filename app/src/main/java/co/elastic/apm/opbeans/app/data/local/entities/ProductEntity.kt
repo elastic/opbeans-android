72dc2d653960766a8161965c3d80514a7e49a653
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-05 11:50:11 +0200

    Created ProductEntity

---
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/local/entities/ProductEntity.kt
@@ -0,0 +1,15 @@
+package co.elastic.apm.opbeans.app.data.local.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product")
+class ProductEntity(
+    @PrimaryKey val id: Int,
+    val sku: String,
+    val name: String,
+    val stock: Int,
+    @ColumnInfo(name = "type_name")
+    val typeName: String
+)
