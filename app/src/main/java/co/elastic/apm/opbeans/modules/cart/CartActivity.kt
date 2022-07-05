2c42e620c576a7a65a466c86220a3b1e1e61b419
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-05 18:05:36 +0200

    Connecting cart repo with viewmodel

--- app/src/main/java/co/elastic/apm/opbeans/modules/cart/CartActivity.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/cart/CartActivity.kt
@@ -1,11 +1,51 @@
 package co.elastic.apm.opbeans.modules.cart
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import co.elastic.apm.opbeans.R
+import co.elastic.apm.opbeans.app.data.models.CartItem
+import co.elastic.apm.opbeans.modules.cart.ui.CartViewModel
+import co.elastic.apm.opbeans.modules.cart.ui.CartViewState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class CartActivity : AppCompatActivity() {
+
+    private val viewModel: CartViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+        initViews()
+
+        lifecycleScope.launch {
+            viewModel.cartState.collectLatest {
+                when (it) {
+                    is CartViewState.Loading -> showLoading()
+                    is CartViewState.ErrorLoading -> showErrorMessage(it.e)
+                    is CartViewState.FinishedLoading -> showCartItems(it.items)
+                }
+            }
+        }
+    }
+
+    private fun initViews() {
+
+    }
+
+    private fun showCartItems(items: List<CartItem>) {
+        TODO("Not yet implemented")
+    }
+
+    private fun showErrorMessage(e: Throwable) {
+
+    }
+
+    private fun showLoading() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/co/elastic/apm/opbeans/modules/cart/ui/CartViewModel.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/cart/ui/CartViewModel.kt
@@ -1,6 +1,25 @@
 package co.elastic.apm.opbeans.modules.cart.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.elastic.apm.opbeans.app.data.repository.CartItemRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-class CartViewModel : ViewModel() {
+@HiltViewModel
+class CartViewModel @Inject constructor(cartItemRepository: CartItemRepository) :
+    ViewModel() {
+
+    val cartState: StateFlow<CartViewState> = cartItemRepository.getAllCartItems()
+        .catch { e -> CartViewState.ErrorLoading(e) }
+        .map { CartViewState.FinishedLoading(it) }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000), CartViewState.Loading
+        )
 }
