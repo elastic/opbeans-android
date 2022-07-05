ae1761c8c4f3b4f1eb2f85291bc1a0e61ee85340
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-05 11:16:44 +0200

    Created cart ui and data classes

---
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/models/CartItem.kt
@@ -0,0 +1,6 @@
+package co.elastic.apm.opbeans.app.data.models
+
+data class CartItem(
+    val product: Product,
+    val amount: Int
+)
---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/cart/ui/CartViewModel.kt
@@ -0,0 +1,6 @@
+package co.elastic.apm.opbeans.modules.cart.ui
+
+import androidx.lifecycle.ViewModel
+
+class CartViewModel : ViewModel() {
+}
