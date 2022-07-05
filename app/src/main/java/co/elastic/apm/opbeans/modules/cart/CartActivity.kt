9402119c401672a251ca3934f882e8c7a4e4d5e4
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-05 10:34:26 +0200

    Created CartActivity

---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/cart/CartActivity.kt
@@ -0,0 +1,11 @@
+package co.elastic.apm.opbeans.modules.cart
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
