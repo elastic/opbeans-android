ee6fc12f13d543113724ba0515ea6e21c4c0188a
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-06 16:34:36 +0200

    Updated CreateOrderBody

--- app/src/main/java/co/elastic/apm/opbeans/app/data/remote/body/CreateOrderBody.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/remote/body/CreateOrderBody.kt
@@ -5,5 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class CreateOrderBody(
     @SerializedName("customer_id")
     val customerId: Int,
+    @SerializedName("lines")
     val products: List<OrderLine>
 )
