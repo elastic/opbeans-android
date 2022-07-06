0c040ed10789843e0610e3b41569882f72842dfc
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-06 11:02:10 +0200

    Setting up product detail activity launch

--- app/src/main/java/co/elastic/apm/opbeans/modules/productdetail/ProductDetailActivity.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/productdetail/ProductDetailActivity.kt
@@ -1,13 +1,34 @@
 package co.elastic.apm.opbeans.modules.productdetail
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.elastic.apm.opbeans.R
 
 class ProductDetailActivity : AppCompatActivity() {
 
+    companion object {
+        private const val PARAM_PRODUCT_ID = "product_id"
+
+        fun launch(context: Context, productId: Int) {
+            val intent = Intent(context, ProductDetailActivity::class.java)
+            intent.putExtra(PARAM_PRODUCT_ID, productId)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        val productId = getProductId()
+    }
+
+    private fun getProductId(): Int {
+        val param = intent.getIntExtra(PARAM_PRODUCT_ID, -1)
+        if (param == -1) {
+            throw IllegalArgumentException("No product id available")
+        }
+        return param
     }
 }
