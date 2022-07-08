f19bcbd17add57c1a078701c34506cbcc184acec
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 11:26:07 +0200

    Providing local orders from orders repo

--- app/src/main/java/co/elastic/apm/opbeans/app/data/repository/OrderRepository.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/repository/OrderRepository.kt
@@ -2,18 +2,30 @@ package co.elastic.apm.opbeans.app.data.repository
 
 import co.elastic.apm.opbeans.app.data.models.CartItem
 import co.elastic.apm.opbeans.app.data.models.Order
+import co.elastic.apm.opbeans.app.data.source.order.LocalOrderSource
 import co.elastic.apm.opbeans.app.data.source.order.RemoteOrderSource
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 
 @Singleton
-class OrderRepository @Inject constructor(private val remoteOrderSource: RemoteOrderSource) {
+class OrderRepository @Inject constructor(
+    private val remoteOrderSource: RemoteOrderSource,
+    private val localOrderSource: LocalOrderSource
+) {
 
     suspend fun createOrder(customerId: Int, items: List<CartItem>) {
         remoteOrderSource.createOrder(customerId, items)
     }
 
-    suspend fun getOrders(): List<Order> {
-        return remoteOrderSource.getOrders()
+    fun getOrders(): Flow<List<Order>> {
+        return localOrderSource.getOrders()
+    }
+
+    suspend fun fetchOrders() = withContext(Dispatchers.IO) {
+        val remoteOrders = remoteOrderSource.getOrders()
+        localOrderSource.saveAll(remoteOrders)
     }
 }
