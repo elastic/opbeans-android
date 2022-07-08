e66f38aa59e2f8562b5de2b1783afac1c80d5142
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 14:46:42 +0200

    Created order detail activity

---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/OrderDetailActivity.kt
@@ -0,0 +1,13 @@
+package co.elastic.apm.opbeans.modules.orderdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OrderDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
