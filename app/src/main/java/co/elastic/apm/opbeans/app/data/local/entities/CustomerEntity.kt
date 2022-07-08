5abf3508115e9a5c0e5d23a0e62f7e6228135eac
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 14:30:05 +0200

    Fixed customer entity primary key

--- app/src/main/java/co/elastic/apm/opbeans/app/data/local/entities/CustomerEntity.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/local/entities/CustomerEntity.kt
@@ -2,10 +2,11 @@ package co.elastic.apm.opbeans.app.data.local.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "customer")
 data class CustomerEntity(
-    val id: Int,
+    @PrimaryKey val id: Int,
     @ColumnInfo(name = "full_name")
     val fullName: String,
     @ColumnInfo(name = "company_name")
