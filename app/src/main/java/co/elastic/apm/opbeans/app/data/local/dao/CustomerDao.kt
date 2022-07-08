c29928409bd49b0e1e8d733e38d460bbed35a676
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 14:20:28 +0200

    Using local source from customer repository

--- app/src/main/java/co/elastic/apm/opbeans/app/data/local/dao/CustomerDao.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/local/dao/CustomerDao.kt
@@ -2,6 +2,7 @@ package co.elastic.apm.opbeans.app.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import co.elastic.apm.opbeans.app.data.local.entities.CustomerEntity
 
@@ -11,6 +12,6 @@ interface CustomerDao {
     @Query("SELECT * FROM customer LIMIT :offset, :amount")
     suspend fun getSetOfCustomers(offset: Int, amount: Int): List<CustomerEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertAll(customers: List<CustomerEntity>)
 }
--- app/src/main/java/co/elastic/apm/opbeans/app/data/repository/CustomerRepository.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/repository/CustomerRepository.kt
@@ -1,14 +1,26 @@
 package co.elastic.apm.opbeans.app.data.repository
 
 import co.elastic.apm.opbeans.app.data.models.Customer
+import co.elastic.apm.opbeans.app.data.source.customer.LocalCustomerSource
 import co.elastic.apm.opbeans.app.data.source.customer.RemoteCustomerSource
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @Singleton
-class CustomerRepository @Inject constructor(private val remoteCustomerSource: RemoteCustomerSource) {
+class CustomerRepository @Inject constructor(
+    private val remoteCustomerSource: RemoteCustomerSource,
+    private val localCustomerSource: LocalCustomerSource
+) {
 
-    suspend fun getCustomers(): List<Customer> {
-        return remoteCustomerSource.getCustomers()
+    suspend fun getSetOfCustomers(offset: Int, amount: Int): List<Customer> =
+        withContext(Dispatchers.IO) {
+            localCustomerSource.getSetOfCustomers(offset, amount)
+        }
+
+    suspend fun fetchCustomers() = withContext(Dispatchers.IO) {
+        val remoteCustomers = remoteCustomerSource.getCustomers()
+        localCustomerSource.saveAll(remoteCustomers)
     }
 }
