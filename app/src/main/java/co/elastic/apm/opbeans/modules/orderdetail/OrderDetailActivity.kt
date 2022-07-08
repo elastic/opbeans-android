7155a2c3bf7559c6a0902a65691dd42cfaf2545f
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 15:33:33 +0200

    Created layout for order details

--- app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/OrderDetailActivity.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/OrderDetailActivity.kt
@@ -2,6 +2,7 @@ package co.elastic.apm.opbeans.modules.orderdetail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import co.elastic.apm.opbeans.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,6 @@ class OrderDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_detail)
     }
 }
