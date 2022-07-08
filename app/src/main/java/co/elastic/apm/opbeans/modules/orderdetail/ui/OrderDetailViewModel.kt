066e0bef9987bc3019d81743d942fca3616a127a
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 15:25:45 +0200

    Created order detail viewmodel

---
+++ app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/ui/OrderDetailViewModel.kt
@@ -0,0 +1,10 @@
+package co.elastic.apm.opbeans.modules.orderdetail.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OrderDetailViewModel @Inject constructor() : ViewModel() {
+
+}
