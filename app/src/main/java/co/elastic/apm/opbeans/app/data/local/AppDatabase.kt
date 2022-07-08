44cf4d258c4d8bb0e4c3fa7c4cae0b75a83c4fd6
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 11:06:29 +0200

    Created OrderDao

--- app/src/main/java/co/elastic/apm/opbeans/app/data/local/AppDatabase.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/local/AppDatabase.kt
@@ -3,16 +3,19 @@ package co.elastic.apm.opbeans.app.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import co.elastic.apm.opbeans.app.data.local.dao.CartItemDao
+import co.elastic.apm.opbeans.app.data.local.dao.OrderDao
 import co.elastic.apm.opbeans.app.data.local.dao.ProductDao
 import co.elastic.apm.opbeans.app.data.local.entities.CartItemEntity
+import co.elastic.apm.opbeans.app.data.local.entities.OrderEntity
 import co.elastic.apm.opbeans.app.data.local.entities.ProductEntity
 
 @Database(
-    entities = [CartItemEntity::class, ProductEntity::class],
+    entities = [CartItemEntity::class, ProductEntity::class, OrderEntity::class],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cartItemDao(): CartItemDao
     abstract fun productDao(): ProductDao
+    abstract fun orderDao(): OrderDao
 }
---
+++ app/src/main/java/co/elastic/apm/opbeans/app/data/local/dao/OrderDao.kt
@@ -0,0 +1,9 @@
+package co.elastic.apm.opbeans.app.data.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface OrderDao {
+
+
+}
