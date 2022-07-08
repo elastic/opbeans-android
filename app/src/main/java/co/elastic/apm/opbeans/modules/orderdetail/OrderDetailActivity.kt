941083e35e21db5545e2e22de8a3c3ea5fa5bdad
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 16:21:06 +0200

    Applying order details data onto UI

--- app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/OrderDetailActivity.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/OrderDetailActivity.kt
@@ -1,15 +1,87 @@
 package co.elastic.apm.opbeans.modules.orderdetail
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.elastic.apm.opbeans.R
+import co.elastic.apm.opbeans.app.ui.ListDivider
+import co.elastic.apm.opbeans.app.ui.LoadableList
+import co.elastic.apm.opbeans.modules.orderdetail.data.OrderDetailStateItem
+import co.elastic.apm.opbeans.modules.orderdetail.ui.OrderDetailState
+import co.elastic.apm.opbeans.modules.orderdetail.ui.OrderDetailViewModel
+import co.elastic.apm.opbeans.modules.orderdetail.ui.list.OrderedProductListAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class OrderDetailActivity : AppCompatActivity() {
 
+    private val viewModel: OrderDetailViewModel by viewModels()
+    private lateinit var list: LoadableList
+    private lateinit var total: TextView
+    private lateinit var adapter: OrderedProductListAdapter
+    private var orderId: Int = -1
+
+    companion object {
+        private const val PARAM_ORDER_ID = "order_id"
+
+        fun launch(context: Context, orderId: Int) {
+            val intent = Intent(context, OrderDetailActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_detail)
+        initViews()
+        initList()
+        setOrderId()
+
+        lifecycleScope.launch {
+            viewModel.state.collectLatest {
+                when (it) {
+                    is OrderDetailState.Loading -> list.showLoading()
+                    is OrderDetailState.ErrorLoading -> list.showError(it.e)
+                    is OrderDetailState.FinishedLoading -> populateData(it.value)
+                }
+            }
+        }
+
+        viewModel.fetchOrderDetail(orderId)
+    }
+
+    private fun setOrderId() {
+        orderId = intent.getIntExtra(PARAM_ORDER_ID, -1)
+        if (orderId == -1) {
+            throw IllegalArgumentException("No order id found")
+        }
+    }
+
+    private fun initViews() {
+        list = findViewById(R.id.list)
+        total = findViewById(R.id.order_detail_total)
+    }
+
+    private fun initList() {
+        adapter = OrderedProductListAdapter()
+        val recyclerView = list.getList()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(ListDivider(this))
+        recyclerView.adapter = adapter
+
+        list.onRefreshRequested { viewModel.fetchOrderDetail(orderId) }
+    }
+
+    private fun populateData(value: OrderDetailStateItem) {
+        total.text = value.totalPrice
+        list.showList()
+        adapter.submitList(value.products)
     }
 }
