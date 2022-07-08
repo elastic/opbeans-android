774959187a2c2e9fb735a26fecf6dc46698b0239
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-07-08 16:10:16 +0200

    Fetching order details from order details view model

--- app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/ui/OrderDetailViewModel.kt
+++ app/src/main/java/co/elastic/apm/opbeans/modules/orderdetail/ui/OrderDetailViewModel.kt
@@ -1,12 +1,34 @@
 package co.elastic.apm.opbeans.modules.orderdetail.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import co.elastic.apm.opbeans.modules.orderdetail.data.usecases.OrderDetailStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 @HiltViewModel
-class OrderDetailViewModel @Inject constructor(private val orderDetailStateUseCase: OrderDetailStateUseCase) : ViewModel() {
+class OrderDetailViewModel @Inject constructor(private val orderDetailStateUseCase: OrderDetailStateUseCase) :
+    ViewModel() {
 
+    private val internalState = MutableStateFlow<OrderDetailState>(OrderDetailState.Loading)
+    val state = internalState.asStateFlow()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), OrderDetailState.Loading)
 
+    fun fetchOrderDetail(orderId: Int) {
+        viewModelScope.launch {
+            try {
+                internalState.update { OrderDetailState.Loading }
+                val order = orderDetailStateUseCase.getOrderDetail(orderId)
+                internalState.update { OrderDetailState.FinishedLoading(order) }
+            } catch (e: Throwable) {
+                internalState.update { OrderDetailState.ErrorLoading(e) }
+            }
+        }
+    }
 }
