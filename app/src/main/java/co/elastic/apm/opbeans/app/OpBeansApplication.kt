7c8eeffbb6fe19699b97f77d476edbbafb5f7e05
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-29 16:42:29 +0200

    Initializing ElasticApmAgent to export to local host

--- app/src/main/java/co/elastic/apm/opbeans/app/OpBeansApplication.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/OpBeansApplication.kt
@@ -1,8 +1,17 @@
 package co.elastic.apm.opbeans.app
 
 import android.app.Application
+import co.elastic.apm.android.sdk.ElasticApmAgent
+import co.elastic.apm.android.sdk.traces.connectivity.Connectivity
+import co.elastic.apm.opbeans.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class OpBeansApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val connectivity = Connectivity.create(BuildConfig.EXPORTER_URL)//.withAuthToken("token")
+        ElasticApmAgent.initialize(this, connectivity)
+    }
 }
