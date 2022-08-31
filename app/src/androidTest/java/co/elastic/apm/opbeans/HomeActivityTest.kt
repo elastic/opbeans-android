0bf060cb43e33aa29c8d69bd16b6665c2168a63a
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-08-31 18:16:13 +0200

    Added simple test for single product checkout

---
+++ app/src/androidTest/java/co/elastic/apm/opbeans/HomeActivityTest.kt
@@ -0,0 +1,118 @@
+package co.elastic.apm.opbeans
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import co.elastic.apm.opbeans.utils.DispatcherIdlerRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class HomeActivityTest {
+
+    @Rule
+    @JvmField
+    val activityScenarioRule = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Rule
+    @JvmField
+    val dispatcherIdlerRule = DispatcherIdlerRule()
+
+    @Test
+    fun testOneProductCheckout() {
+        val recyclerView = onView(
+            allOf(
+                withId(R.id.list),
+                childAtPosition(
+                    withClassName(`is`("android.widget.FrameLayout")),
+                    0
+                )
+            )
+        )
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.add_to_cart_fab), withContentDescription("Add to cart"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
+
+        pressBack()
+
+        val actionMenuItemView = onView(
+            allOf(
+                withId(R.id.shopping_cart), withContentDescription("Shopping Cart"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(androidx.appcompat.R.id.action_bar),
+                        1
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        actionMenuItemView.perform(click())
+
+        val actionMenuItemView2 = onView(
+            allOf(
+                withId(R.id.cart_checkout_option), withContentDescription("Checkout"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(androidx.appcompat.R.id.action_bar),
+                        2
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        actionMenuItemView2.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
