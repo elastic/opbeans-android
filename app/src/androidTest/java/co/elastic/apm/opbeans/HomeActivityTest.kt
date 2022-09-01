584e8b6dc0e1953e049a0d5625c3f0f377472e1c
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-09-01 09:33:49 +0200

    Clean up tests

--- app/src/androidTest/java/co/elastic/apm/opbeans/HomeActivityTest.kt
+++ app/src/androidTest/java/co/elastic/apm/opbeans/HomeActivityTest.kt
@@ -1,8 +1,6 @@
 package co.elastic.apm.opbeans
 
 
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
@@ -10,19 +8,12 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withClassName
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import co.elastic.apm.opbeans.utils.DispatcherIdlerRule
 import co.elastic.apm.opbeans.utils.EspressoUtils
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -41,84 +32,18 @@ class HomeActivityTest {
 
     @Test
     fun testOneProductCheckout() {
-        val recyclerView = onView(
-            allOf(
-                withId(R.id.list),
-                childAtPosition(
-                    withClassName(`is`("android.widget.FrameLayout")),
-                    0
-                )
-            )
-        )
-        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+        onView(withId(R.id.list)).perform(actionOnItemAtPosition<ViewHolder>(0, click()))
 
-        val floatingActionButton = onView(
-            allOf(
-                withId(R.id.add_to_cart_fab), withContentDescription("Add to cart"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(android.R.id.content),
-                        0
-                    ),
-                    3
-                ),
-                isDisplayed()
-            )
-        )
-        floatingActionButton.perform(click())
+        onView(withId(R.id.add_to_cart_fab)).perform(click())
 
         pressBack()
 
-        val actionMenuItemView = onView(
-            allOf(
-                withId(R.id.shopping_cart), withContentDescription("Shopping Cart"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(androidx.appcompat.R.id.action_bar),
-                        1
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        actionMenuItemView.perform(click())
+        onView(withId(R.id.shopping_cart)).perform(click())
 
-        val actionMenuItemView2 = onView(
-            allOf(
-                withId(R.id.cart_checkout_option), withContentDescription("Checkout"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(androidx.appcompat.R.id.action_bar),
-                        2
-                    ),
-                    0
-                ),
-                isDisplayed()
-            )
-        )
-        actionMenuItemView2.perform(click())
+        onView(withId(R.id.cart_checkout_option)).perform(click())
 
         EspressoUtils.waitForView(withId(R.id.empty_list_container))
 
         onView(withId(R.id.empty_list_message)).check(matches(isDisplayed()))
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
