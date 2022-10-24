f26d8e5c38105d7995832def2a76bcf9eb50fc0a
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-10-24 16:59:05 +0200

    Revert "Setting connectivity and service name from gradle extension"
    This reverts commit 305f903f

--- app/src/main/java/co/elastic/apm/opbeans/app/OpBeansApplication.kt
+++ app/src/main/java/co/elastic/apm/opbeans/app/OpBeansApplication.kt
@@ -20,6 +20,8 @@ package co.elastic.apm.opbeans.app
 
 import android.app.Application
 import co.elastic.apm.android.sdk.ElasticApmAgent
+import co.elastic.apm.android.sdk.traces.connectivity.Connectivity
+import co.elastic.apm.opbeans.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -27,6 +29,11 @@ class OpBeansApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        ElasticApmAgent.initialize(this)
+        val connectivity = Connectivity.create(BuildConfig.EXPORTER_URL).apply {
+            if (BuildConfig.EXPORTER_AUTH_TOKEN.isNotEmpty()) {
+                withAuthToken(BuildConfig.EXPORTER_AUTH_TOKEN)
+            }
+        }
+        ElasticApmAgent.initialize(this, connectivity)
     }
 }
