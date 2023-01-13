3a7806b6cfd4b7914ae7975be1d0aca0ba183c3a
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-13 15:47:29 +0100

    Clean up

--- app/src/main/java/co/elastic/apm/opbeans/app/tools/OpbeansGlideModule.kt
+++
@@ -1,8 +0,0 @@
-package co.elastic.apm.opbeans.app.tools
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class OpbeansGlideModule : AppGlideModule() {
-}
