f9f99e22b4e4457aff03586bfb501e3b37ca90ff
Cesar Munoz <cesar.munozquintero@elastic.co>
2023-01-13 15:34:27 +0100

    Added Glide app module

---
+++ app/src/main/java/co/elastic/apm/opbeans/app/tools/OpbeansGlideModule.kt
@@ -0,0 +1,8 @@
+package co.elastic.apm.opbeans.app.tools
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class OpbeansGlideModule : AppGlideModule() {
+}
