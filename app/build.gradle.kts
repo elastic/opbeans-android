d1d4a5b129d40fbe060af806d4c6bc206756de03
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2025-10-28 12:12:48 +0100

    Updating header format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,7 @@ spotless {
 
 fun getSourceHeader(): String {
     return """
+        /* 
         Licensed to Elasticsearch B.V. under one or more contributor
         license agreements. See the NOTICE file distributed with
         this work for additional information regarding copyright
@@ -116,5 +117,6 @@ fun getSourceHeader(): String {
         KIND, either express or implied.  See the License for the
         specific language governing permissions and limitations
         under the License. 
+        */
     """.trimIndent()
 }
